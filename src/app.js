3b84e11278e065b81b48481ad4645b0dbc592b23
mackingsley <kingsleyudoma2018@gmail.com>
2025-07-03 15:30:09 +0100

    push

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/home",function(req,res){
+
+    res.status(200).json({
+        status:200,
+        message:"Welcome to Mars"
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("App is running on port 3000");
+})
